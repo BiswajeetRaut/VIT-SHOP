b159ad8187d1de22a7562c5f75b30d67b94ec6a0
BiswajeetRaut <85820671+BiswajeetRaut@users.noreply.github.com>
2023-02-05 15:20:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import firebase from 'firebase';
+import Header from './components/Header';
+function App() {
+  return (
+    <div className="App">
+      {/* Header */}
+      <Header></Header>
+      {/* Slider */}
+      {/* Items Components */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyB4-VXewJdfrAFToFACVSPvQ7nBWMFT9Cw",
+  authDomain: "vit-shop.firebaseapp.com",
+  projectId: "vit-shop",
+  storageBucket: "vit-shop.appspot.com",
+  messagingSenderId: "1083362229702",
+  appId: "1:1083362229702:web:94f990b80fa8c702f486c7",
+  measurementId: "G-SVF4PQDY13"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
+
